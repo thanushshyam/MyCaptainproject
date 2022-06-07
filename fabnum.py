f04645fa7276245d3c9703edad3377c58bec5a7d
thanushshyam <106827796+thanushshyam@users.noreply.github.com>
2022-06-07 18:34:31 +0530

    Add files via upload

---
+++ fabnum.py
@@ -0,0 +1,14 @@
+Number = int(input("Enter the Range No.: "))
+i = 0
+value1 = 0
+value2 = 1
+while(i < Number):
+           if(i <= 1):
+                      Next = i
+           else:
+                      Next = value1 + value2
+                      value1 = value2
+                      value2 = Next
+           print(Next)
+           i = i + 1
+
