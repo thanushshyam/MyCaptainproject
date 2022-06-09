153824dbed27b4f4700f407bd6093bdd2cbc692e
thanushshyam <106827796+thanushshyam@users.noreply.github.com>
2022-06-09 11:00:44 +0530

    Add files via upload

---
+++ 0+veNumInRange.py
@@ -0,0 +1,8 @@
+#to print positive Numbers in a given range
+ 
+start = int(input("Enter the start of range: "))
+end = int(input("Enter the end of range: "))
+ 
+for num in range(start, end + 1):
+    if num >= 1:
+        print(num, end = " ")
