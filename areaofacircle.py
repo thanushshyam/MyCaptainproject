ef2fccf9b33fbb7858518a676a69c429c6d85141
thanushshyam <106827796+thanushshyam@users.noreply.github.com>
2022-06-15 21:18:01 +0530

    Update areaofacircle.py

--- areaofacircle.py
+++ areaofacircle.py
@@ -1,9 +1,11 @@
+#to accept a filename from the user and print the extension of that
+FileName= input("Enter Filename: ")
+F = FileName.split(".")
+print ("Extension of the file is : " + F[-1])
+
 #to print area of a circle
-
 Name = float(input("Input the radius of the circle(in cms): "))
-
 Area = 3.14*(Name*Name)
-
 print("The area of the circle with radius is: ", Area)
 
 
